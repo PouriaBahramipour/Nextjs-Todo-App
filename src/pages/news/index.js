7d404978bba57d17da9db56e0b425cbbe3073305
pouria bahramipour <p@bahramipour.com>
2023-04-11 21:19:51 +0330

    add pages

---
+++ src/pages/news/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+const News = () => {
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <Link href="news/something else">Next is a Greate Framework</Link>
+    </Fragment>
+  );
+};
+
+export default News;
---
+++ src/pages/news/newsPage2.js
@@ -0,0 +1,5 @@
+const NewsPage2 = () => {
+  return <h1>News Page 2</h1>;
+};
+
+export default NewsPage2;
