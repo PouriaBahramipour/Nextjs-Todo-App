ce67de90224d453b9ba78ecd205451a896aba277
pouria bahramipour <p@bahramipour.com>
2023-04-12 00:01:03 +0330

    add pages

--- src/pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const Details = () => {
-  const router = useRouter();
-  const newsId = router.query.newsId;
-  return <h1>Details Page: {newsId}</h1>;
-};
-
-export default Details;
--- src/pages/news/newsPage2.js
+++
@@ -1,5 +0,0 @@
-const NewsPage2 = () => {
-  return <h1>News Page 2</h1>;
-};
-
-export default NewsPage2;
