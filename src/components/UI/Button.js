71cec78c8c7073b28ebb41659e59efa6aa16ffc7
pouria bahramipour <pouryabahramipoor@gmail.com>
2023-04-12 16:29:36 +0330

    create add task component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,15 @@
+import classes from "./Button.module.scss";
+
+const Button = (props) => {
+  return (
+    <button
+      className={`${classes.button} ${props.className}`}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
