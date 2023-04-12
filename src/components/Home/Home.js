17f9a45ad5bf4055a228ebb9ad72a73b869f9cac
pouria bahramipour <pouryabahramipoor@gmail.com>
2023-04-12 15:05:25 +0330

    create home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import Card from "../UI/Card";
+import HomeImage from "./HomeImage";
+import classes from "./Home.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <Card>
+      <div className={classes.home}>
+        <div>
+          <HomeImage />
+        </div>
+        <div className={classes.title}>
+          <p>There’s no task here!</p>
+        </div>
+        <div className={classes["new-task-link"]}>
+          <FontAwesomeIcon
+            className={classes.plusIcon}
+            icon={faPlus}
+            size="1x"
+            color="#1B91FB"
+          />
+          <Link href="/new-task">Create new task</Link>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default Home;
