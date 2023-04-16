65948e233ea7d13132f7e5c4a77fc784b8169c4e
pouria bahramipour <pouryabahramipoor@gmail.com>
2023-04-17 03:27:13 +0330

    edit

--- src/components/Tasks/TaskItems/TaskItems.js
+++ src/components/Tasks/TaskItems/TaskItems.js
@@ -6,7 +6,7 @@ const TaskItems = () => {
   const todoItems = useSelector((state) => state.todo.filteredData);
 
   const tasksList = todoItems.map((task) => (
-    <TaskItemLists id={task.id} title={task.title} category={task.category} />
+    <TaskItemLists key={task.id} title={task.title} category={task.category} />
   ));
 
   return <ul className={classes.taskItem}>{tasksList}</ul>;
